ad454f3a8e4257c625baa80689d1433175b9e46f
Abdullah Kuyucak <42abdullah.kuyucak09@gmail.com>
2021-01-17 00:33:37 +0300

    engin demiroğ yazılım geliştirme kampı

---
+++ ConsoleApp1/SepetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class SepetManager
+    {
+        public void Ekle(Product product)
+        {
+            Console.WriteLine("Sepete eklendi : "+ product.Adi);
+
+        }
+        public void Ekle2(string urunAdi,string urunAciklamasi,double urunFiyati)
+
+        {
+            Console.WriteLine("Sepete eklendi"+urunAdi);
+        }
+    }
+}
